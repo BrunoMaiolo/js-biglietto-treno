af540445e3eea83356b852399ec040509fd5e4dd
BrunoMaiolo <brunosj@hotmail.it>
2025-12-16 15:10:00 +0100

    Richiesta all'utente dell'eta'

--- main.js
+++ main.js
@@ -3,3 +3,6 @@ console.log("JS collegato");
 // Richiesta dei KM da percorrere
 const kilometers = parseFloat(prompt("Quanti chilometri vuoi percorrere?"));
 console.log("Km inseriti:", kilometers);
+
+// Richiesta dell'eta' 
+const age = parseInt(prompt("Quanti anni hai?"));
