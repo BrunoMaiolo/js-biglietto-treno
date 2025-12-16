9672e2ebdcb36ce0f682b58d7fe588df9620333a
BrunoMaiolo <brunosj@hotmail.it>
2025-12-16 15:08:50 +0100

    Stampo Km da percorrere su CONSOLE

--- main.js
+++ main.js
@@ -2,3 +2,4 @@ console.log("JS collegato");
 
 // Richiesta dei KM da percorrere
 const kilometers = parseFloat(prompt("Quanti chilometri vuoi percorrere?"));
+console.log("Km inseriti:", kilometers);
